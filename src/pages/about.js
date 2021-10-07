dc206125b4e9953ffe025c56043c7ee7b3899603
Paul Thompson <pt@sevenelm.com>
2021-10-06 22:35:39 -0400

    added for finished module 2

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+// Step 1: Import React
+import * as React from 'react'
+import Layout from '../components/layout'
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="'Bout Page">
+    <p>I think this considered "children" 'bout</p>
+    </Layout>
+  )
+}
+// Step 3: Export your component
+export default AboutPage
